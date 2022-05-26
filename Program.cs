114fdc82fa059f4856c1c980d2308d4da055e0ef
Daniil <daniil.goihinberg@gmail.com>
2022-05-26 20:23:45 +0300

    my homework

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FortuneCookie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cookie = { "win a jackpot", "fall in love", "get a love letter", "miss the bus", "see a unicorn on the street" };
+
+            Random rnd = new Random();
+            int randomCookie = rnd.Next(0, cookie.Length);
+
+            Console.WriteLine($"Cookie saying you {cookie[randomCookie]}");
+        }
+    }
+}
